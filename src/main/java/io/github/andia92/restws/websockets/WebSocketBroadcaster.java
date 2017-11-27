7b48a89450e33af90778ad6e8beb50245ca862eb
Andrea Aczel <aczelandi@gmail.com>
2017-11-27 22:49:59 +0200

    Add integration test

--- src/main/java/io/github/andia92/restws/websockets/WebSocketBroadcaster.java
+++ src/main/java/io/github/andia92/restws/websockets/WebSocketBroadcaster.java
@@ -1,12 +1,13 @@
 package io.github.andia92.restws.websockets;
 
 import io.github.andia92.restws.exceptions.ChannelNotFoundException;
+
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONObject;
+import spark.Spark;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -19,7 +20,11 @@ public class WebSocketBroadcaster {
     private static Map<String, WebSocketHandler> wsRegistry = new ConcurrentHashMap<>();
 
     public static void main(String[] args) {
+        WebSocketBroadcaster broadcaster = new WebSocketBroadcaster();
+        broadcaster.run(args);
+    }
 
+    protected void run(String[] args) {
         stream(args)
                 .forEach(arg -> {
                     log.info("Create web socket at route: " + arg);
@@ -37,12 +42,20 @@ public class WebSocketBroadcaster {
                     .orElseThrow(() -> new ChannelNotFoundException("Could not find handler for route: " + route))
                     .dispatch(req.body());
 
-            return new JSONObject("{status: 'success'}");
+            resp.type("application/json");
+
+            return "{\"status\": \"success\"}";
         });
 
         exception(ChannelNotFoundException.class, (exception, request, response) -> {
             log.error("Bad request: ", exception);
             halt(HttpServletResponse.SC_BAD_REQUEST);
         });
+
+        awaitInitialization();
+    }
+
+    protected void stop() {
+        Spark.stop();
     }
 }
---
+++ src/test/java/io/github/andia92/restws/websockets/WebSocketBroadcasterTest.java
@@ -0,0 +1,109 @@
+package io.github.andia92.restws.websockets;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
+import org.eclipse.jetty.websocket.client.WebSocketClient;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@Slf4j
+public class WebSocketBroadcasterTest {
+
+    private static final String CHANNEL = "test1";
+
+    private String destUri = "ws://localhost:4567/" + CHANNEL;
+
+    private URI echoUri = new URI(destUri);
+
+    private WebSocketBroadcaster webSocketBroadcaster;
+
+    private WebSocketClient wsClient;
+
+    private WebSocketTestImpl webSocket;
+
+    public WebSocketBroadcasterTest() throws URISyntaxException {
+    }
+
+    @Before
+    public void before() {
+        webSocketBroadcaster = new WebSocketBroadcaster();
+        webSocketBroadcaster.run(new String[]{CHANNEL});
+
+        wsClient = new WebSocketClient();
+        initWS();
+    }
+
+    private void initWS() {
+        try {
+            wsClient.start();
+            webSocket = new WebSocketTestImpl();
+            ClientUpgradeRequest request = new ClientUpgradeRequest();
+            wsClient.connect(webSocket, echoUri, request);
+            log.info("Connecting to : %s%n", echoUri);
+        } catch (Throwable t) {
+            log.error("Exception occurred:", t.getMessage());
+        }
+    }
+
+    @Test
+    public void restApiCalled_MessageDispatchedToWS() throws InterruptedException {
+        given().body("content")
+               .when()
+               .post("http://localhost:4567/broadcast/test1")
+               .then()
+               .statusCode(200)
+               .body("status", equalTo("success"));
+
+        List<String> actual = new ArrayList<>();
+
+        while (actual.size() == 0) {
+            actual = webSocket.getMessages();
+        }
+
+        List<String> expected = Collections.singletonList("content");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @After
+    public void after() throws Exception {
+        webSocketBroadcaster.stop();
+        wsClient.stop();
+    }
+
+    @WebSocket
+    public static class WebSocketTestImpl {
+
+        @Getter
+        private final List<String> messages = new ArrayList<>();
+
+        @OnWebSocketConnect
+        public void onConnect(Session user) throws Exception {
+        }
+
+        @OnWebSocketClose
+        public void onClose(Session user, int statusCode, String reason) {
+        }
+
+        @OnWebSocketMessage
+        public void onMessage(Session user, String message) {
+            messages.add(message);
+        }
+    }
+}
